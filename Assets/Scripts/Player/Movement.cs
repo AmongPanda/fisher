931c311f7412bf6cfb30099ea736cb343a7370ff
Trebieno <dfyz.pfbrby2@gmail.com>
2023-01-24 15:10:33 +0300

    Add movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _maxSpeed = 30f;
+    [SerializeField] private float _minSpeed = 10f;
+    [SerializeField] private float _curSpeed = 0f;
+    [SerializeField] private float _timeAcceleration;
+    [SerializeField] private SpriteRenderer _skinBoat;
+    private Rigidbody2D _rb;
+
+    private bool _move;
+    private bool _left;
+    private void Start() 
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        if(_move && _left)
+        {
+            if(_curSpeed <= _maxSpeed)
+                _curSpeed += Time.deltaTime/_timeAcceleration;
+            Vector2 direction = new Vector3(1, 0);
+            _rb.AddForce(direction * _curSpeed);
+            _rb.MoveRotation(15);
+            _skinBoat.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        
+        if(_move && !_left)
+        {
+            if(_curSpeed <= _maxSpeed)
+                _curSpeed += Time.deltaTime/_timeAcceleration;
+            Vector2 direction = new Vector3(-1, 0);
+            _rb.AddForce(direction * _curSpeed);
+            _skinBoat.transform.rotation = new Quaternion(0, 180, 0, 0);
+            _rb.MoveRotation(-15);
+        }
+
+        if(!_move && _curSpeed > 0)
+        {
+            _curSpeed -= Time.deltaTime;
+        }
+    }
+
+    private void ResetSpeed()
+    {
+        
+    }
+
+    public void Move(bool move) => _move = move;
+    public void Direction(bool direction) => _left = direction;
+
+    
+}  
